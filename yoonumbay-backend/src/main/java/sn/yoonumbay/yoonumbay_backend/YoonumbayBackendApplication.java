2f9b0078b0c01f662ede7dce4b7d5030bc845b84
eadarak00 <eadarak2001@gmail.com>
2026-01-20 22:06:28 +0000

    Feat: TASK-1: initialisation du projet Spring Boot

---
+++ yoonumbay-backend/src/main/java/sn/yoonumbay/yoonumbay_backend/YoonumbayBackendApplication.java
@@ -0,0 +1,13 @@
+package sn.yoonumbay.yoonumbay_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonumbayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonumbayBackendApplication.class, args);
+	}
+
+}
