e46504396a036ed39dee3c9c7a47b94d2cbce1ed
eadarak00 <eadarak2001@gmail.com>
2026-01-20 22:57:43 +0000

    feat: configuration du  doc

---
+++ yoonumbay-backend/src/main/java/sn/yoonumbay/yoonumbay_backend/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package sn.yoonumbay.yoonumbay_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI yoonumbayOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("YoonuMbay API")
+                        .description("API backend de la plateforme YoonuMbay")
+                        .version("1.0.0"));
+    }
+}
