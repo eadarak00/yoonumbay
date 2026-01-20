62fb5fcff6358f9e2d895e978f63bd4d5766861e
eadarak00 <eadarak2001@gmail.com>
2026-01-20 22:15:35 +0000

    chore: initialisation du projet Spring Boot

---
+++ yoonumbay-backend/src/test/java/sn/yoonumbay/yoonumbay_backend/YoonumbayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.yoonumbay.yoonumbay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoonumbayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
