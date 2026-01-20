b5008d35dee7e2c7fbf670a88ea452b711742788
eadarak00 <eadarak2001@gmail.com>
2026-01-20 22:55:59 +0000

    feat: ajout du dependances de l'audit

--- yoonumbay-backend/src/main/java/sn/yoonumbay/yoonumbay_backend/YoonumbayBackendApplication.java
+++ yoonumbay-backend/src/main/java/sn/yoonumbay/yoonumbay_backend/YoonumbayBackendApplication.java
@@ -2,8 +2,10 @@ package sn.yoonumbay.yoonumbay_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YoonumbayBackendApplication {
 
 	public static void main(String[] args) {
